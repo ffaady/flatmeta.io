7a4ef214a8e043f077e9c259726ce5e39097487d
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-04-22 15:55:02 +0500

    my place API and UI udpate

--- src/app/pages/myplaces/myplaces.page.ts
+++ src/app/pages/myplaces/myplaces.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/providers/storage.service';
+import { GeneralService } from 'src/app/providers/general.service';
+import { HttpService } from 'src/app/providers/http.service';
 
 @Component({
   selector: 'app-myplaces',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyplacesPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public storage: StorageService,
+    public general: GeneralService,
+    public http: HttpService
+  ) { }
+
+  tilesList = [];
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.getList()
+  }
+
+  getList() {
+    this.http.get2('TileDetails', true).subscribe((res: any) => {
+      this.general.stopLoading();
+      console.log(res);
+      if(res.status == true){
+        this.tilesList = res.data;
+      }
+    }, (e) => {
+      this.general.stopLoading();
+      console.log(e)
+    })
   }
 
 }
