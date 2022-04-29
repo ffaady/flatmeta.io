98a5b92309b0416dd0d37808d8c8102fe4fe21b9
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-04-29 16:38:31 +0500

    custom Popup Added for my self.

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -162,15 +162,32 @@ export class HomePage implements OnInit {
     const coordinates = await Geolocation.getCurrentPosition();
     let ico = GlobaldataService.userObject != undefined ? GlobaldataService.userObject.avatar : 'https://avataaars.io/?avatarStyle=Transparent&topType=Hat&accessoriesType=Kurt&facialHairType=BeardLight&facialHairColor=Red&clotheType=BlazerShirt&eyeType=Dizzy&eyebrowType=Default&mouthType=Default&skinColor=Light';
     const icon = Leaflet.icon({
-      iconUrl:  ico,
+      iconUrl: ico,
       iconSize: [50, 50], // size of the icon
       popupAnchor: [13, 0],
     });
+
+    let customPopup = `
+      <p>Hello i am ${GlobaldataService.userObject.full_name}<p>
+      <p>i am Selling my Place you can Buy it. 
+      <div class="flex">
+        <ion-button size="small">Message</ion-button>
+        <ion-button size="small">Request</ion-button>
+      </div>      
+    `;
+
+    // specify popup options 
+    let customOptions = {
+      'maxWidth': '400',
+      'width': '200',
+      'className': 'popupCustom',
+    }
+
     const marker = new DriftMarker([coordinates.coords.latitude, coordinates.coords.longitude], {
       draggable: true,
       icon: icon
     })//@ts-ignore
-      .addTo(this.map);
+      .bindPopup(customPopup, customOptions).addTo(this.map);
 
     const onMapClick = (e) => {
       marker.slideTo(e.latlng, { duration: 1500 });
