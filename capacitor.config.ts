11cc1f0e6b32561eb18adc00b2e9e2eaeca3b961
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-05-23 18:33:05 +0500

    android Update, and IOS added.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.leaflet.example',
-  appName: 'leafletmap',
+  appName: 'Flatmeta',
   webDir: 'www',
   bundledWebRuntime: false
 };
