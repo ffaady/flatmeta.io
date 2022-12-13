97d6644eb4e1f0d5da60e5bbd97f99ee74bed799
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-12-13 15:11:05 +0500

    recent purchases list and API

--- src/app/pages/recentpurchases/recentpurchases.page.ts
+++ src/app/pages/recentpurchases/recentpurchases.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/providers/general.service';
+import { HttpService } from 'src/app/providers/http.service';
+import { EventsService } from 'src/app/providers/events.service';
 
 @Component({
   selector: 'app-recentpurchases',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecentpurchasesPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public general: GeneralService,
+    public http: HttpService,
+    public events: EventsService
+  ) { }
+
+  tilesList = [];
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+    this.getList()
+  }
+
+  getList() {
+    this.http.get('LatestPurchasedTiles', true).subscribe((res: any) => {
+      this.general.stopLoading();
+      if(res.status == true){
+        this.tilesList = res.data.tiles;
+      }
+    }, (e) => {
+      this.general.stopLoading();
+      console.log(e)
+    })
+  }
+
+  toUserPlace(c){
+    setTimeout(()=>{
+      this.events.publishToUser(c);
+    })
   }
 
 }
